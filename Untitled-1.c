05b216a03062e2a2ee2137a50322b9df06f7f89e
Abdelrahman74S <bdalrhmnr927@gmail.com>
2023-08-30 00:35:24 +0300

    variable

--- Untitled-1.c
+++ Untitled-1.c
@@ -14,9 +14,11 @@ scanf("%d",&nam1);
 printf("please give number");
 scanf("%d",&nam2);
 
-sum = nam1 * nam2 ;
+sum = nam1 % nam2 ;
 
 printf("sum %d and %d = %d",nam1,nam2,sum);
 
 }
+
+
  
---
+++ variable.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+   
+
+
+
+
+
+void main (void)
+
+{
+
+unsigned char op;
+int num;
+float sal;
+double dis;
+ 
+op = 200;
+num = 230;
+sal = 680;
+dis = 10075758.95189669;
+
+printf("%c\n",op);
+
+printf("%d\n",num);
+
+printf("%f\n",sal);
+
+printf("%lf\n",dis);
+ 
+} 
