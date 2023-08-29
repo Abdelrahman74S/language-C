e5d4ddc1297fab4c5b047150d4ef89ced74db3a0
Abdelrahman74S <bdalrhmnr927@gmail.com>
2023-08-29 03:33:32 +0300

    the first code

---
+++ Untitled-1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+void main (void)
+{
+
+int nam1,nam2;
+int sum;
+
+printf("please give number");
+scanf("%d",&nam1);
+
+ 
+printf("please give number");
+scanf("%d",&nam2);
+
+sum = nam1 * nam2 ;
+
+printf("sum %d and %d = %d",nam1,nam2,sum);
+
+}
+ 
